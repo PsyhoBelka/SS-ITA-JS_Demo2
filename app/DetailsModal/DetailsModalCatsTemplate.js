062fba77020d8feaf593191a406bc88f609e3493
PsyhoBelka <agroghkov@gmail.com>
2019-12-22 16:32:41 +0200

    fix details modal template

--- app/DetailsModal/DetailsModalCatsTemplate.js
+++ app/DetailsModal/DetailsModalCatsTemplate.js
@@ -3,22 +3,42 @@ export class DetailsModalCatsTemplate {
   static getHTML({ breed, price, gender, weight, birth_date, color, image, is_sterile, hair }) {
     // language=HTML
     return `
-        <div class="uk-modal-dialog uk-modal-body">
+        <div class="uk-modal-dialog uk-modal-body uk-padding-small">
             <button class="modal_close uk-modal-close-default uk-flex-right" type="button" uk-close></button>
             <h2 class="details_title uk-modal-title uk-flex uk-flex-center">${breed}</h2>
             <div class="details_photo uk-flex uk-flex-center">
                 <img src="${image}" onerror="this.onerror = null; this.src='assets/No-image-available.png';">
             </div>
-            <div class="details_body uk-modal-body uk-flex uk-flex-row uk-flex-around">
-                <div class="uk-flex uk-flex-column uk-flex-inline">
-                    Gender: ${gender}<br>
-                    Age: ${((Date.now() - birth_date) / 1000 / 60 / 60 / 24 / 365).toFixed(0)} yr<br>
-                    Sterile: ${is_sterile ? 'Yes' : 'No'}<br>
-                    Weigth: ${weight} <br>
-                    Color: ${color} <br>
-                    Hair: ${hair}
-                </div>
-                <div class="uk-flex uk-flex-column uk-flex-inline ">
+            <div class="details_body uk-modal-body uk-flex uk-flex-row uk-flex-around uk-padding-small">
+                <table class="details_table uk-table uk-table-small uk-table-divider uk-width-small uk-table-middle">
+                            <tbody>
+                            <tr>
+                                <td >Gender</td>
+                                <td >${gender}</td>
+                            </tr>
+                            <tr>
+                                <td >Age</td>
+                                <td >${((Date.now() - birth_date) / 1000 / 60 / 60 / 24 / 365).toFixed(0)} yr</td>
+                            </tr>
+                            <tr>
+                                <td >Weight</td>
+                                <td >${weight}kg</td>
+                            </tr>
+                            <tr>
+                                <td >Color</td>
+                                <td >${color}</td>
+                            </tr>
+                            <tr>
+                                <td >Sterile</td>
+                                <td >${is_sterile ? 'Yes' : 'No'}</td>
+                            </tr>
+                            <tr>
+                                <td >Hair</td>
+                                <td >${hair}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                <div class="uk-flex uk-flex-inline uk-flex-middle">
                     <h2 class="uk-heading-small">$${price.toFixed(2)}</h2>
                 </div>
             </div>
--- app/DetailsModal/DetailsModalFishesTemplate.js
+++ app/DetailsModal/DetailsModalFishesTemplate.js
@@ -1,35 +1,56 @@
 export class DetailsModalFishesTemplate {
 
-  static getHTML() {
+  static getHTML({ breed, price, gender, weight, birth_date, color, image, water_type, temper }) {
     // language=HTML
     return `
-        <div class="uk-modal-dialog uk-modal-body">
-            <button class="uk-modal-close-default uk-flex-right" type="button" uk-close></button>
-            <h2 class="details_title uk-modal-title uk-flex uk-flex-center">Siberian husky</h2>
-            <div class="details_photo">
-                <img src="assets/cat-1.jpg" onerror="this.onerror = null; this.src='assets/No-image-available.png';">
+        <div class="uk-modal-dialog uk-modal-body uk-padding-small">
+            <button class="modal_close uk-modal-close-default uk-flex-right" type="button" uk-close></button>
+            <h2 class="details_title uk-modal-title uk-flex uk-flex-center">${breed}</h2>
+            <div class="details_photo uk-flex uk-flex-center">
+                <img src="${image}" onerror="this.onerror = null; this.src='assets/No-image-available.png';">
             </div>
-            <div class="details_body uk-modal-body uk-flex uk-flex-row uk-flex-around">
-                <div class="uk-flex uk-flex-column uk-flex-inline">
-                    Gender:<br>
-                    Age:<br>
-                    Sterile:<br>
-                    body
+            <div class="details_body uk-modal-body uk-flex uk-flex-row uk-flex-around uk-padding-small">
+                <table class="details_table uk-table uk-table-small uk-table-divider uk-width-small uk-table-middle">
+                            <tbody>
+                            <tr>
+                                <td >Gender</td>
+                                <td >${gender}</td>
+                            </tr>
+                            <tr>
+                                <td >Age</td>
+                                <td >${((Date.now() - birth_date) / 1000 / 60 / 60 / 24 / 365).toFixed(0)} yr</td>
+                            </tr>
+                            <tr>
+                                <td >Weight</td>
+                                <td >${weight}g</td>
+                            </tr>
+                            <tr>
+                                <td >Color</td>
+                                <td >${color}</td>
+                            </tr>
+                            <tr>
+                                <td >Water type</td>
+                                <td >${water_type}</td>
+                            </tr>
+                            <tr>
+                                <td >Temper</td>
+                                <td >${temper}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                <div class="uk-flex uk-flex-inline uk-flex-middle">
+                    <h2 class="uk-heading-small">$${price.toFixed(2)}</h2>
                 </div>
-                <div class="uk-flex uk-flex-column uk-flex-inline ">
-                    <h2 class="uk-heading-small">$344</h2>
-                </div>
-
             </div>
             <div class="uk-flex uk-flex-around">
                 <div class="uk-flex uk-flex-left uk-flex-inline">
-                    <div class="clickable uk-button uk-button-danger uk-padding-small uk-padding-remove-vertical">
+                    <div class="modal_details__buy_button clickable uk-button uk-button-danger uk-padding-small uk-padding-remove-vertical">
                         Buy pet
                     </div>
                 </div>
                 <div class="uk-flex uk-flex-right uk-flex-inline">
-                    <div class="uk-button uk-button-secondary">
-                        <div class="clickable uk-modal-close uk-padding-small uk-padding-remove-vertical">Close
+                    <div class="modal_close uk-button uk-button-secondary uk-modal-close">
+                        <div class="clickable uk-padding-small uk-padding-remove-vertical">Close
                         </div>
                     </div>
                 </div>
